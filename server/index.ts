5cda8cc5e91b396c4fdf7cb729591e3aa917f7a2
maccollinsfsu <maccollins44@gmail.com>
2025-12-05 22:12:04 -0500

    Add logging to error handler

--- server/index.ts
+++ server/index.ts
@@ -48,7 +48,15 @@ app.use((req, res, next) => {
     const message = err.message || "Internal Server Error";
 
     res.status(status).json({ message });
-    throw err;
+    log(
+      JSON.stringify({
+        level: "error",
+        status,
+        message,
+        stack: err?.stack,
+      }),
+      "express",
+    );
   });
 
   // importantly only setup vite in development and after
